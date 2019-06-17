220045ac987364fd6c99e9c48cae2eb542216c57
MengGP <oleg.ustsov@gmail.com>
2019-06-17 19:36:40 +0700

    upd build, сборка для всех субпроектов, ++ server-core: menggp.server.Main

---
+++ server-core/src/main/java/menggp/server/Main.java
@@ -0,0 +1,7 @@
+package menggp.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server-core - HELLO !!!");
+    } // end_main
+} // end_class
